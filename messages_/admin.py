45b9c40303377b88bec60e14c8ba2ee75f498b50
zateyev <zhasulan.ateyev@gmail.com>
2016-10-17 18:18:55 +0700

    flake8 configuration

--- messages_/admin.py
+++ messages_/admin.py
@@ -3,5 +3,3 @@ from django.contrib import admin
 # Register your models here.
 from messages_.models import Message
 admin.site.register([Message])
-
-
--- messenger/settings.py
+++ messenger/settings.py
@@ -56,8 +56,7 @@ ROOT_URLCONF = 'messenger.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,7 +77,7 @@ WSGI_APPLICATION = 'messenger.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
